eed5eb2c9f49ca5bac71dd1b75897e5f3229cb19
k024g0083 <k024g0083@g.neec.ac.jp>
2024-10-25 08:19:40 +0900

    プレイヤーの構造体を作り、初期値を入れた

--- main.cpp
+++ main.cpp
@@ -2,6 +2,62 @@
 
 const char kWindowTitle[] = "宗てぃんぐ";
 
+
+/*-----------------
+    構造体を作る
+-----------------*/
+
+// 位置
+struct Pos2
+{
+	float x;
+	float y;
+};
+
+// ベクトル
+struct Vec2
+{
+	float x;
+	float y;
+};
+
+// 移動速度
+struct Vel2
+{
+	float x;
+	float y;
+};
+
+// 復活
+struct Respawn
+{
+	// 復活しているかどうか（復活フラグ）
+	int isRespawn;
+
+	// 復活処理
+	int timer;
+};
+
+// プレイヤー
+struct Player
+{
+	// 復活
+	struct Respawn respawn;
+
+	// 位置
+	struct Pos2 pos;
+
+	// ベクトル
+	struct Vec2 vec;
+
+	// 移動速度
+	struct Vel2 vel;
+
+	// 図形の半径
+	float radius;
+};
+
+
 /*---------------
     定数を作る
 ---------------*/
@@ -22,6 +78,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+
+	/*-----------------------------
+	    変数を作り、初期値を入れる
+	-----------------------------*/
+
+	/*   プレイヤー   */
+
+	// 構造体
+	struct Player player;
+
+	// 復活
+	player.respawn.isRespawn = true;
+	player.respawn.timer = 120;
+
+	// 位置
+	player.pos = { 0.0f , 0.0f };
+
+	// ベクトル
+	player.vec = { 0.0f , 0.0f };
+
+	// 移動速度
+	player.vel = { 0.0f , 0.0f };
+
+	// 図形の半径
+	player.radius = 0.0f;
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
